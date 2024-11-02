c374d29d30fe81b2d5cdb3cc1357bc624cad291e
Marc Yebra <marcyebra4@gmail.com>
2024-11-01 23:15:14 -0500

    react components re-organized

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LandingPage from "./LandingPage";
+import LandingPage from "./landing/LandingPage";
 
 function App() {
   return (
--- app/javascript/components/landing/LandingPage.js
+++ app/javascript/components/landing/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ThreeModel from './ThreeModel';
+import BlogShow from '../blog/BlogShow';
 
 
 const LandingPage = () => {
